3dc317fb5b1e3c852ca9d357535b7e11262fcb48
Danyl Fernandes <realllydan@gmail.com>
2022-06-05 19:52:28 +0530

    :wrench: Extend tailwind colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: [
-    './**/*.html',
-  ],
+  content: ['./**/*.html'],
   theme: {
+    colors: {
+      'accent-red': '#DC1616',
+      ...colors,
+    },
     extend: {
       fontFamily: {
         sans: ['Barlow', ...defaultTheme.fontFamily.sans],
-      }
+        mono: ['Space Grotesk', ...defaultTheme.fontFamily.mono],
+      },
     },
   },
   plugins: [],
-}
+};
