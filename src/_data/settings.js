8a5dc15035545017f06e0221f8dad676253c0469
Danyl Fernandes <realllydan@gmail.com>
2022-06-05 19:53:22 +0530

    :sparkles: Add website settings data

---
+++ src/_data/settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  author: 'Danyl Fernandes',
+  nav: {
+    title: 'dan',
+    status: 'is building 🛠️',
+  },
+  social: {
+    twitter: 'https://twitter.com/androidanyl',
+    github: 'https://github.com/gitryder',
+    stackOverflow: '',
+    devto: '',
+    quora: '',
+    linkedIn: 'https://www.linkedin.com/in/danyl-fernandes-5bb706157/',
+  },
+};
