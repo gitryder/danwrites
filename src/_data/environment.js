f261b643c631c0d32b7afe1c7e2a190a541c848c
Danyl Fernandes <realllydan@gmail.com>
2022-06-07 14:45:00 +0530

    :wrench: Add fallback base url

--- src/_data/environment.js
+++ src/_data/environment.js
@@ -1,4 +1,6 @@
 module.exports = {
-  baseUrl: `${process.env.BASE_URL}`,
+  // The fallback variable used here - process.env.URL
+  // is the env variable provided by Netlify.
+  baseUrl: `${process.env.BASE_URL || process.env.URL}`,
   nodeEnv: process.env.NODE_ENV,
 }
